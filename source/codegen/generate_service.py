946b88275c3a9e840240b65e0168a8e8d29803ed
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-09-01 10:19:34 -0500

    Temp fixes for metadata mutation and validation.

--- source/codegen/generate_service.py
+++ source/codegen/generate_service.py
@@ -22,7 +22,7 @@ def _generate_service_file(metadata, template_file_name, generated_file_suffix, 
 
 def _mutate_metadata(metadata: dict):
     config = metadata["config"]
-    if config["driver_name"] in []:
+    if config["driver_name"] in ["NI-DCPower"]:
         return
 
     metadata_mutation.move_zero_enums_to_front(metadata["enums"])
