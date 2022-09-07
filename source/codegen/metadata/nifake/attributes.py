cbcfe660e6a78d136076d61ea2c3af7ae3f3133e
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-09-07 16:17:00 -0500

    Get 'resettable' back on attributes.

--- source/codegen/metadata/nifake/attributes.py
+++ source/codegen/metadata/nifake/attributes.py
@@ -5,18 +5,21 @@ attributes = {
         'codegen_method': 'public',
         'grpc_type': 'bool',
         'name': 'READ_WRITE_BOOL',
+        'resettable': False,
         'type': 'ViBoolean'
     },
     1000001: {
         'codegen_method': 'public',
         'grpc_type': 'double',
         'name': 'READ_WRITE_DOUBLE',
+        'resettable': False,
         'type': 'ViReal64'
     },
     1000002: {
         'codegen_method': 'public',
         'grpc_type': 'string',
         'name': 'READ_WRITE_STRING',
+        'resettable': False,
         'type': 'ViString'
     },
     1000003: {
@@ -24,12 +27,14 @@ attributes = {
         'enum': 'Color',
         'grpc_type': 'sint32',
         'name': 'READ_WRITE_COLOR',
+        'resettable': False,
         'type': 'ViInt32'
     },
     1000004: {
         'codegen_method': 'public',
         'grpc_type': 'sint32',
         'name': 'READ_WRITE_INTEGER',
+        'resettable': True,
         'type': 'ViInt32'
     },
     1000005: {
@@ -37,36 +42,42 @@ attributes = {
         'enum': 'FloatEnum',
         'grpc_type': 'double',
         'name': 'FLOAT_ENUM',
+        'resettable': False,
         'type': 'ViReal64'
     },
     1000006: {
         'codegen_method': 'public',
         'grpc_type': 'int64',
         'name': 'READ_WRITE_INT64',
+        'resettable': False,
         'type': 'ViInt64'
     },
     1000007: {
         'codegen_method': 'public',
         'grpc_type': 'double',
         'name': 'READ_WRITE_DOUBLE_WITH_CONVERTER',
+        'resettable': False,
         'type': 'ViReal64'
     },
     1000008: {
         'codegen_method': 'public',
         'grpc_type': 'sint32',
         'name': 'READ_WRITE_INTEGER_WITH_CONVERTER',
+        'resettable': False,
         'type': 'ViInt32'
     },
     1000009: {
         'codegen_method': 'public',
         'grpc_type': 'double',
         'name': 'READ_WRITE_DOUBLE_WITH_REPEATED_CAPABILITY',
+        'resettable': False,
         'type': 'ViReal64'
     },
     1000010: {
         'codegen_method': 'public',
         'grpc_type': 'string',
         'name': 'READ_WRITE_STRING_REPEATED_CAPABILITY',
+        'resettable': False,
         'type': 'ViString'
     }
 }
