044246bb57aca1dcfcfb9bfea6ac75c91f049eb8
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-09-07 16:39:50 -0500

    Add additional_headers to config.py

--- source/codegen/metadata/nifake/config.py
+++ source/codegen/metadata/nifake/config.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # This file is generated from NI-FAKE API metadata version 22.8.0d9999
 config = {
+    'additional_headers': {
+    },
     'api_version': '22.8.0d9999',
     'c_function_prefix': 'niFake_',
     'c_header': 'niFake.h',
